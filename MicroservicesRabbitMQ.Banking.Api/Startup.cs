2b409fa3b6873ecc6c2b36a36b9f808e73f543f5
Kevin Guedes <kevinguedes@edu.unifor.br>
2021-07-16 14:18:49 -0300

    Removed unecessary imports

--- MicroservicesRabbitMQ.Banking.Api/Startup.cs
+++ MicroservicesRabbitMQ.Banking.Api/Startup.cs
@@ -21,7 +21,6 @@ namespace MicroservicesRabbitMQ.Banking.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             AddConfigurations(services);
@@ -48,7 +47,6 @@ namespace MicroservicesRabbitMQ.Banking.Api
             DependencyResolver.RegisterServices(services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
